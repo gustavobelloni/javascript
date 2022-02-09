452b1a230f6aba466cff09609d1804f59eed1b20
Gustavo Pereira Belloni <98773683+gustavobelloni@users.noreply.github.com>
2022-02-09 14:03:10 -0300

    Criei ex008 até 0014

---
+++ aulas/aula12ex/ex014/java/script.js
@@ -0,0 +1,22 @@
+function carregar() {
+    var msg = window.document.querySelector('div#msg')
+    var img = window.document.querySelector('img#imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora < 12) {
+        img.src = 'imagens/manha.png'
+        document.body.style.background = '#C7E9FD'
+    } else if (hora < 18) {
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#3D95FF'
+    } else {
+        img.src = 'imagens/noite.png'
+        document.body.style.background = '#072239'
+    }
+}
+
+     
+
+
