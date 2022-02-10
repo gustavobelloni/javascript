c0c751c1629fc5e0749b2fe90b96ab97320f8c0b
Gustavo Pereira Belloni <98773683+gustavobelloni@users.noreply.github.com>
2022-02-10 11:20:19 -0300

    Criei o exercício 16 e 17

---
+++ aulas/aula14ex/ex016/java/script.js
@@ -0,0 +1,30 @@
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar'
+        //window.alert('[ERRO] Faltam dados!')
+        
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Consederando PASSO 1')
+        }
+        if (i < f ) {
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{27A1} `
+            }
+        } else {
+            for(let c = i; c >= f ; c -= p) {
+            res.innerHTML += ` ${c} \u{27A1} `
+            }
+        }
+    res.innerHTML += `\u{2714}`
+    }
+}
